d8da71d3af33c31276e7a7c3b92d618558adaae3
gorillaradio <spitruzzello@gmail.com>
2026-04-09 19:10:26 +0200

    feat: create EnrollmentFeeFactory and add HasFactory to EnrollmentFee model

--- app/Models/EnrollmentFee.php
+++ app/Models/EnrollmentFee.php
@@ -3,13 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Stancl\Tenancy\Database\Concerns\BelongsToTenant;
 
 class EnrollmentFee extends Model
 {
-    use HasUuids, BelongsToTenant;
+    use HasUuids, HasFactory, BelongsToTenant;
 
     protected $fillable = ['student_id', 'payment_id', 'expected_amount', 'starts_at', 'expires_at', 'notes'];
 
